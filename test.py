ac759f4e4d713a46add5c59eb796fc96060af280
mina <mina@gmail.com>
2024-12-07 17:25:24 +0200

    modifing function sum , divide , subrtract >> solving string errors

--- test.py
+++ test.py
@@ -1,18 +1,12 @@
-def sum ():
-    counter =0
-    for y in x :
-       counter+=y
-    print(counter)
+def sum (x,y):
+    return int(x)+int(y)
 def  subtract (x, y):
-    return x - y
-def divide ():
-    pass
-x = {1,2,3,4,5,6}
-for i in x:
-    print(i,'\n')
+    return int(x)-int(y)
+def divide (x,y):
+    return int(x)/int(y)
 
 
-sum()
+
 num1 = input("enter first number\n")
 num2 = input("enter second number\n")
 print( subtract(num1 , num2))
