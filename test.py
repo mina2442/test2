a357c06334298c1830fb52ee1c5b7d4a0686cb0a
mina <mina@gmail.com>
2024-12-07 17:20:15 +0200

    taking 2 values ofrom user and subtracting them

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ def sum ():
     print(counter)
 def  subtract (x, y):
     return x - y
-
+def divide ():
+    pass
 x = {1,2,3,4,5,6}
 for i in x:
     print(i,'\n')
 
 
 sum()
-
-q = subtract(20 , 5)
-print(q)
+num1 = input("enter first number\n")
+num2 = input("enter second number\n")
+print( subtract(num1 , num2))
