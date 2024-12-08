7afd1040f2708cfb92b8081a0029e0fa74b26ff2
mina <mina@gmail.com>
2024-12-08 16:33:27 +0200

    alot of changes

---
+++ c.py
@@ -0,0 +1,9 @@
+class car :
+    speed = 0
+    model = 0
+    color = 0
+
+
+
+    def speed (self):
+        print (self.speed)
--- class.py
+++
@@ -1,9 +0,0 @@
-class car ():
-    speed = 0
-    model = 0
-    color = 0
-
-
-
-    def speed ():
-        print (speed)
