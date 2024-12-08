3d7d5775136b737723b07deb31e7d27537190701
mina <mina@gmail.com>
2024-12-08 17:42:35 +0200

    adding 4 functions for speed

--- c.py
+++ c.py
@@ -5,5 +5,14 @@ class Car:
 
 
 
-    def speed(self):
+    def speed1(self):
+        print(self.speed)
+
+    def speed2(self):
+        print(self.speed)
+
+    def speed3(self):
+        print(self.speed)
+
+    def speed4(self):
         print(self.speed)
